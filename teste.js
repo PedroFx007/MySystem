07ccc0582f3909ebdc3a4dec8d06580b0a9bef1c
PedroFx007 <73764705+PedroFx007@users.noreply.github.com>
2020-12-09 23:27:41 -0300

    Add files via upload

---
+++ teste.js
@@ -0,0 +1,11 @@
+var os = require('os');
+
+
+console.clear();
+
+
+console.log('=================')
+console.log('seu celular é um:')
+console.log(os.platform());
+console.log('=================')
+
